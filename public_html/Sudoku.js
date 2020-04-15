a75c4f79d8ffd0ddb707a57c6d79dd86ea45fcc5
larei <larei@lreich-x360>
2020-04-15 15:47:47 -0400

    Removed jQuery jQuery dependency was removed by directly accessing the DOM. This is much easier with newer browsers. This will not work in IE or Edge because they do not correctly handle "getElementsByName".

--- public_html/Sudoku.js
+++ public_html/Sudoku.js
@@ -17,10 +17,10 @@
  */
 
 function solveSudoku(grid) {
-    var loc = {};
+    let loc = {};
     
     if(findEmptyGridSlot(loc, grid) === true) {
-        for(var num = 1; num <= 9; num++) {
+        for(let num = 1; num <= 9; num++) {
             if(canPlaceNum(loc.row, loc.col, num, grid)) {
                 grid[loc.row][loc.col] = num;
                 if(solveSudoku(grid))
@@ -35,8 +35,8 @@ function solveSudoku(grid) {
 }
 
 function findEmptyGridSlot(loc, grid) {
-    for(var r = 0; r <= 8; r++) {
-        for(var c = 0; c <= 8; c++) {
+    for(let r = 0; r <= 8; r++) {
+        for(let c = 0; c <= 8; c++) {
             if(grid[r][c] === 0) {
                 loc.row = r;
                 loc.col = c;
@@ -49,20 +49,20 @@ function findEmptyGridSlot(loc, grid) {
 
 function canPlaceNum(row, col, num, grid) {
     // Check row
-    for(var i = 0; i <= 8; i++) {
+    for(let i = 0; i <= 8; i++) {
         if(grid[i][col] === num)
             return false;
     }
     // Check col
-    for(var i = 0; i <= 8; i++) {
+    for(let i = 0; i <= 8; i++) {
         if(grid[row][i] === num)
             return false;
     }
     // Check small grid
     var bigRow = ~~(row / 3);
     var bigCol = ~~(col / 3);
-    for(var r = bigRow * 3; r <= (bigRow * 3) + 2; r++) {
-        for(var c = bigCol * 3; c <= (bigCol * 3) + 2; c++) {
+    for(let r = bigRow * 3; r <= (bigRow * 3) + 2; r++) {
+        for(let c = bigCol * 3; c <= (bigCol * 3) + 2; c++) {
             if(grid[r][c] === num)
                 return false;
         }
@@ -70,9 +70,9 @@ function canPlaceNum(row, col, num, grid) {
     return true;
 }
 
-$(document).ready(function() {
+function init() {
     
-    var grid = [
+    let grid = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -83,83 +83,86 @@ $(document).ready(function() {
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
-
-    $('body').append($('<div>').addClass('wrapper')
-            .append($('<div>').addClass('col')
-            .append($('<h1>').html('Sudoku Solver'))
-            .append(generateSudokuGrid())))
-            .append($('<button>').html('Solve'));
-
-    $('table[class^="sudoku"]').each(function () {
-        populateGrid(this, grid);
-    });
     
-    $('table[class^="numGrid"]').each(function () {
-        populateNumTable(this);
-    });
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    document.body.appendChild(wrapper);
     
-    $('button').click( function () {
-        $('table[class^="sudoku"').each(function () {
-            readData(this, grid);
-        });
+    const col = document.createElement('div');
+    col.classList.add('col');
+    wrapper.appendChild(col);
+    
+    const head = document.createElement('h1');
+    head.innerHTML = 'Sudoku Solver';
+    col.appendChild(head);
+    
+    const table = document.createElement('table');
+    for(let i=0; i<9; i++) {
+        const row = table.insertRow(-1); // Append a row to the table
+        for(let j=0; j<9; j++) {
+            const cell = row.insertCell(-1); // Append a cell to the row
+            // Set up event listener on each cell
+            cell.addEventListener('click', event => {
+                let current;
+                document.getElementsByName('num').forEach(element => {
+                    if(element.checked === true) {
+                        current = parseInt(element.id);
+                    }
+                });
+                if(current !== 0) {
+                    cell.innerText = current;
+                }
+                else {
+                    cell.innerText = '';
+                }
+            });
+        }
+    }
+    table.classList.add('sudoku');
+    col.appendChild(table);
+    
+    const solve = document.createElement('button');
+    solve.classList.add('solve');
+    solve.innerHTML = 'Solve';
+    document.body.appendChild(solve);
+    
+    populateGrid(table, grid);
+    
+    solve.addEventListener('click', (event) => {
+        readData(table, grid);
         solveSudoku(grid);
-        $('table[class^="sudoku"]').each(function () {
-            populateGrid(this, grid);
+        populateGrid(table, grid);
+    });
+    
+    document.getElementsByName('num').forEach(element => {
+        element.addEventListener('click', event => {
+            // Clear all radio buttons
+            document.getElementsByName('num').forEach(radio => {
+                radio.setAttribute('checked', false);
+            });
+            element.setAttribute('checked', true);
         });
     });
     
-    $('input[type="checkbox"]').click( function () {
-        $('input[type="radio"]').each(function () {
-            $(this).prop('checked', false);
-        });
-        $(this).prop('checked', true);
-    });
-    
-    $('td').click( function () {
-        var current;
-        $('input[type="radio"]').each(function () {
-            if($(this).is(':checked')) {
-                current = parseInt($(this).attr('id'));
-            }
-        });
-        if(current !== 0)
-            $(this).text(current);
-        else
-            $(this).text('');
-    });
-});
+}
 
 function populateGrid(g, grid) {
-    $(g).find('tr').each(function (row) {
-        $(this).find('td').each(function (col) {
-            $(this).text(grid[row][col] || '');
-        });
-    });
+    for(let row=0; row<9; row++) {
+        for(let col=0; col<9; col++) {
+            g.rows[row].cells[col].innerText = grid[row][col] || '';
+        }
+    }
 }
 
 function readData(g, grid) {
-    $(g).find('tr').each(function (row) {
-        $(this).find('td').each(function (col) {
-            if($(this).text() !== '')
-                grid[row][col] = parseInt($(this).text());
-            else
+    for(let row=0; row<9; row++) {
+        for(let col=0; col<9; col++) {
+            if(g.rows[row].cells[col].innerText !== '') {
+                grid[row][col] = parseInt(g.rows[row].cells[col].innerText);
+            }
+            else {
                 grid[row][col] = 0;
-        });
-    });
-}
-
-function generateSudokuGrid() {
-    return $('<table>').append(multiPush(9, function () {
-        return $('<tr>').append(multiPush(9, function () {
-            return $('<td>');
-        }));
-    })).addClass('sudoku');
-}
-
-function multiPush(count, func, scope) {
-    var arr = [];
-    for (var i = 0; i < count; i++) {
-        arr.push(func.call(scope, i));
+            }
+        }
     }
-    return arr;
 }
